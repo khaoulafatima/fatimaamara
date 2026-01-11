0419dab0d9fe179885b4ce414518336b7fea3280
khaoulafatima <amafatima.zahra@gmail.com>
2026-01-11 12:04:45 +0100

    up

--- src/pages/Fragments.jsx
+++ src/pages/Fragments.jsx
@@ -71,7 +71,7 @@ const FragmentsComponent = () => {
         display="grid"
         gridTemplateColumns={{
           xs: "1fr",
-          sm: "1fr 1fr",
+          sm: "1fr",
           md: "1fr 1fr", 
         }}
         gap={3}
